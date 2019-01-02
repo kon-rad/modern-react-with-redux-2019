0055fceb983921359909bc08cd3477d9891e0729
konradgnat <konradmgnat@gmail.com>
2019-01-01 20:28:47 -0600

    init

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+  state = { images: [] };
 
-  }
+  onSearchSubmit = async (term) => {
+    const res = await unsplash.get('/search/photos', {
+      params: {query: term},
+    });
+
+    this.setState({ images: res.data.results });
+  };
 
   render() {
     return (
       <div>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images}/>
       </div>
     )
   }
