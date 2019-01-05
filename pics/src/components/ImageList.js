dcc9ccd5805796cbef194f2c7b65bd34dedf187a
konradgnat <konradmgnat@gmail.com>
2019-01-04 23:44:14 -0600

    songs

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 require('./ImageList.css');
 
 const ImageList = (props) => {
-  const images = props.images.map(({ urls, description, id }) => {
-    return <img alt={description} key={id} src={urls.regular} />
-  })
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
+  });
+
   return (
     <div className={"image-list"}>{images}</div>
   )
