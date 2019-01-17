f132525d36f20e7ae55a05a2390a9626f6f5af00
konradgnat <konradmgnat@gmail.com>
2019-01-16 23:45:27 -0600

    edit streams

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
-import history from '../history';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
   return (
